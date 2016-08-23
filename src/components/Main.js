f635907050f0bc753676e99b0039d0975c7f24dc
w20999hhm <w20999hf@qq.com>
2016-08-23 18:28:17 +0800

    app init

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+// let imageDatas = require("../data/imageDatas.json")
+//
+// imageDatas = (function genImageURL(imageDatasArr) {
+//   for (var i = 0; i < imageDatasArr.length; i++) {
+//     let singleImageData = imageDatasArr[i];
+//     singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+//     imageDatasArr[i] = singleImageData;
+//   }
+//   return imageDatasArr;
+// })(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      // <div className="index">
+      //   <img src={yeomanImage} alt="Yeoman Generator"/>
+      //   <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      // </div>
+      <section className="stage">
+        <section className="img-sec">Hello</section>
+        <nav className="controller-nav"></nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {};
+
+export default AppComponent;
